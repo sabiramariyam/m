83ab6acbc3f0c11b10c2e0c66634dd3cedb691a9
sabiramariyam <42670623+sabiramariyam@users.noreply.github.com>
2018-12-30 01:58:57 +0530

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,13 @@
+ch=input()
+
+if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' ):
+
+    print ("vowel")
+
+elif ((ch >= 'a' and ch <='z') or (ch >= 'A' and ch <= 'Z')):
+
+    print ("consonant")
+
+else:
+
+    print("invalid")
