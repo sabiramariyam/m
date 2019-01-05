f3aba1ef62b742189752c36e68311664acbef2b7
sabiramariyam <42670623+sabiramariyam@users.noreply.github.com>
2019-01-05 14:46:44 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main()
+
+{
+    
+char ch;
+    
+scanf("%c",&ch);
+    
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+    
+{
+        
+printf("Alphabet",ch);
+    
+}
+    
+else
+    
+{
+        
+printf("no");
+    
+}
+
+}
+   
+ 
