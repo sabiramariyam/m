a28584ec2036b476d6cf53e7d8dd5b46ab2de6b9
sabiramariyam <42670623+sabiramariyam@users.noreply.github.com>
2019-01-05 14:36:06 +0530

    Delete v.py

--- v.py
+++
@@ -1,7 +0,0 @@
-ch = input()
-if(ch == 'a' or ch == 'e' or ch =='i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
-  print("vowel")
-elif((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z'):
-  print ("consonant")
-else:
-  print("invalid")
