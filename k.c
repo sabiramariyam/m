fa393db2c1e04292d63486e6115e87d6c1d94008
sabiramariyam <42670623+sabiramariyam@users.noreply.github.com>
2019-02-07 22:23:31 -0800

    Add files via upload

---
+++ k.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[]={o,1,2,3,4,5,6,7,8,9};
+int sum=0,n,k,i;
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+printf("%d",a[i]);
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+}
